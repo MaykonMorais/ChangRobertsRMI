5957ad6239b34db0f2114dbd252cf952f86300d5
RenanRibeir <renanribeirow19@gmail.com>
2021-05-17 21:22:28 -0300

    init Election cicle

--- src/interfaces/IConnection.java
+++ src/interfaces/IConnection.java
@@ -12,8 +12,10 @@ import structure.Node;
 public interface IConnection extends Remote {
 	public void registerRMI(Integer port) throws RemoteException, UnknownHostException, AlreadyBoundException;
 	public String hello() throws RemoteException;
-	
-	public void sendMessage(Integer port, String message, Queue<Node> paths) throws RemoteException, NotBoundException,InterruptedException;
+	public void election(String msg,Integer port, Queue<Node> paths)throws RemoteException;
+	public void receiveElection(String message,Queue<Node> paths) throws RemoteException, NotBoundException, InterruptedException;
+
+    public void sendMessage(Integer port, String message, Queue<Node> paths) throws RemoteException, NotBoundException,InterruptedException;
 	public void receiveMessage(String message, Queue<Node> paths) throws RemoteException, NotBoundException, InterruptedException;
 	
 }
--- src/structure/Node.java
+++ src/structure/Node.java
@@ -4,24 +4,13 @@ import java.io.Serializable;
 
 public class Node implements Serializable {
 
-
-
-		private int id;
 	    public boolean leader = false;
 	    private Integer neighbour; // vizinhos - portas
 	    
 	    public Node(int id, boolean leader, Integer vizinhos) {
-	    	setId(id);
 	    	setLeader(leader);
 	    	setNeighbours(vizinhos);
 	    }
-	    
-		public int getId() {
-			return id;
-		}
-		public void setId(int id) {
-			this.id = id;
-		}
 		public boolean isLeader() {
 			return leader;
 		}
